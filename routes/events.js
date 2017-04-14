04aa8a215619ca4345041d1e1e70b9d563b58531
Erica Rottman <erica7650@gmail.com>
2017-04-14 11:33:43 -0700

    routes shell

---
+++ routes/events.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+
+  
+});
+
+
+
+
+module.exports = router;
