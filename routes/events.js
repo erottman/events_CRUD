4ed2bb5a4fb4f79e79d7d99e2e7c741a0f011deb
Erica Rottman <erica7650@gmail.com>
2017-04-15 12:54:29 -0700

    first get all route

--- routes/events.js
+++ routes/events.js
@@ -5,8 +5,13 @@ const db = require('../db');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-
-  
+  db.from('events')
+  .innerJoin('venues','venues.id', 'events.venue_id')
+  .innerJoin('tickets', 'tickets.id', 'tickets.events_id')
+  .innerJoin('attendees_tickets', 'attendees_tickets.ticket_id', 'tickets.id')
+  .then(events => {
+    res.render('events/index', {events});
+  })
 });
 
 
