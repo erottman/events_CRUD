477a405dd0482eb829e221fe713b11d24a2c97be
Erica Rottman <erica7650@gmail.com>
2017-04-17 09:00:07 -0700

    update over21 request

--- routes/events.js
+++ routes/events.js
@@ -14,6 +14,17 @@ router.get('/', (req,res,next) => {
   })
 });
 
+router.get('/:id', (req,res,next) => {
+  let over21 = req.query.over_21
+  console.log(req.query);
+  db.from('events')
+  .where('events.over_21', over21)
+    .then(event => {
+      console.log(event);
+      res.render('events/show', event[0]);
+  })
+});
+
 
 
 router.get('/:id', (req,res,next) => {
@@ -30,23 +41,7 @@ router.get('/:id', (req,res,next) => {
   })
 });
 
-router.get('/:id/over_21', (req,res,next) => {
-  let id = req.params.id
-  let over21 = req.query.over_21
-  console.log(over21);
-  console.log('kittne', req.params.over_21);
-  db.from('events')
-  .innerJoin('venues','events.venue_id', 'venues.id')
-  .innerJoin('tickets','events.id', 'tickets.events_id')
-  .innerJoin('attendees_tickets', 'tickets.id', 'attendees_tickets.ticket_id')
-  .innerJoin('attendees', 'attendees.id', 'attendees_tickets.attendee_id')
-  .where('events.id', id)
-  .and('events.over_21', over21)
-  .first()
-    .then(event => {
-      res.render('events/show', {event});
-  })
-});
+
 
 
 
